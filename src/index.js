466c26076612e3734a14ceeb55743c7343fc8be1
tatsiana_shautsova <tatsiana_shautsova@epam.com>
2018-09-30 23:09:01 +0300

    Task 7

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  
+  let result = 0;  
+  let i = 1;
+  
+  while (number/(5*i) >= 1){    
+    result += Math.trunc(number/(5*i));
+    i *= 5;
+  }
+  return result;
 }
